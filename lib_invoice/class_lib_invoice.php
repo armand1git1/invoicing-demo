b0d29bfc62334445f11295f26f95dc0c1cb5e078
N8419 <n8419@student.jamk.fi>
2021-12-25 21:16:57 +0200

    Clean lib invoice class

--- lib_invoice/class_lib_invoice.php
+++ lib_invoice/class_lib_invoice.php
@@ -71,6 +71,29 @@ class  class_lib_invoiceclass
       return $info;
     }
   }
+
+  /* This function retrieves needed variable from xml and return a simple array  */     
+  function get_arrayfrom_xml($xml_data)                                                     
+  {   
+   $tab_index    =Array();               
+   if (isset($xml_data))
+   { 
+    $i =0;
+    foreach($xml_data as $inv_data) {
+      if(isset($inv_data->BuyerOrganisationUnitNumber))              $tab_index[$i]['BuyerOrganisationUnitNumber']         = $inv_data->BuyerOrganisationUnitNumber."";
+      if(isset($inv_data->BuyerPartyDetails->BuyerOrganisationName)) $tab_index[$i]['BuyerOrganisationName']               = $inv_data->BuyerPartyDetails->BuyerOrganisationName."";
+      if(isset($inv_data->BuyerPartyDetails->BuyerPostalAddressDetails->BuyerTownName)) $tab_index[$i]['BuyerTownName']    = $inv_data->BuyerPartyDetails->BuyerPostalAddressDetails->BuyerTownName."";
+      if(isset($inv_data->BuyerPartyDetails->BuyerPostalAddressDetails->BuyerPostCodeIdentifier)) $tab_index[$i]['BuyerPostCodeIdentiﬁer']    = $inv_data->BuyerPartyDetails->BuyerPostalAddressDetails->BuyerPostCodeIdentifier."";
+      if(isset($inv_data->InvoiceDetails->InvoiceTotalVatIncludedAmount)) $tab_index[$i]['InvoiceTotalVatIncludedAmount']  = $inv_data->InvoiceDetails->InvoiceTotalVatIncludedAmount."";
+
+      $date = new DateTime($inv_data->InvoiceDetails->PaymentTermsDetails->InvoiceDueDate."");
+      
+      if(isset($inv_data->InvoiceDetails->PaymentTermsDetails->InvoiceDueDate)) $tab_index[$i]['InvoiceDueDate']           = $date->format('Y-m-d');;
+      $i++;
+    }   
+   }  
+   return($tab_index);                                      
+  }
  
   // Convert excel file into csv ->xml format for parsing (could be used if we download the files )
  function xlsx_to_csv() {
@@ -94,31 +117,5 @@ class  class_lib_invoiceclass
      $writer->save($file_name.'.csv');
    }    
  }
-
-  /* This function retrieves needed variable from xml and return a simple array  */     
- function get_arrayfrom_xml($xml_data)                                                     // Sorting the tag Sport
- {   
-  $tab_index    =Array();               
-  if (isset($xml_data))
-  { 
-    $i =0;
-    foreach($xml_data as $inv_data) {
-      if(isset($inv_data->BuyerOrganisationUnitNumber))              $tab_index[$i]['BuyerOrganisationUnitNumber']         = $inv_data->BuyerOrganisationUnitNumber."";
-      if(isset($inv_data->BuyerPartyDetails->BuyerOrganisationName)) $tab_index[$i]['BuyerOrganisationName']               = $inv_data->BuyerPartyDetails->BuyerOrganisationName."";
-      if(isset($inv_data->BuyerPartyDetails->BuyerPostalAddressDetails->BuyerTownName)) $tab_index[$i]['BuyerTownName']    = $inv_data->BuyerPartyDetails->BuyerPostalAddressDetails->BuyerTownName."";
-      if(isset($inv_data->BuyerPartyDetails->BuyerPostalAddressDetails->BuyerPostCodeIdentifier)) $tab_index[$i]['BuyerPostCodeIdentiﬁer']    = $inv_data->BuyerPartyDetails->BuyerPostalAddressDetails->BuyerPostCodeIdentifier."";
-      if(isset($inv_data->InvoiceDetails->InvoiceTotalVatIncludedAmount)) $tab_index[$i]['InvoiceTotalVatIncludedAmount']  = $inv_data->InvoiceDetails->InvoiceTotalVatIncludedAmount."";
-
-      $date = new DateTime($inv_data->InvoiceDetails->PaymentTermsDetails->InvoiceDueDate."");
-      
-      if(isset($inv_data->InvoiceDetails->PaymentTermsDetails->InvoiceDueDate)) $tab_index[$i]['InvoiceDueDate']           = $date->format('Y-m-d');;
-      $i++;
-    }   
-  } 
-
- 
-  return($tab_index);                                      
- }
-
 }                                               
 ?>
